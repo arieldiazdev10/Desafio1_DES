f0b8810ad51b64a684e085146e91077c8f45b1fd
arieldiazdev10 <ariel.ernesto.diaz.carpio@gmail.com>
2026-07-21 00:11:00 -0600

    Se ejecuta la migración de la corrección del modelo Departamento

--- Desafio1_DES/Migrations/20260721060740_Migracion de corrección - Creacion del modelo Departamento.Designer.cs
+++ Desafio1_DES/Migrations/20260721060740_Migracion de corrección - Creacion del modelo Departamento.Designer.cs
@@ -4,6 +4,7 @@ using Desafio1_DES.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Desafio1_DES.Migrations
 {
     [DbContext(typeof(DepartamentosDBContext))]
-    partial class DepartamentosDBContextModelSnapshot : ModelSnapshot
+    [Migration("20260721060740_Migracion de corrección - Creacion del modelo Departamento")]
+    partial class MigraciondecorrecciónCreaciondelmodeloDepartamento
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
