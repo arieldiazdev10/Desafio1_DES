f5c66886e0e7a7844246371df134cb0e3780444a
arieldiazdev10 <ariel.ernesto.diaz.carpio@gmail.com>
2026-07-20 23:46:09 -0600

    Creación del modelo Empleado y modificación del contexto de Departamentos

---
+++ Desafio1_DES/Models/Empleado.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Desafio1_DES.Models
+{
+    public class Empleado
+    {
+        public int ID { get; set; }
+
+        [StringLength(100, MinimumLength =2)]
+        [Required]
+        public string NombreEmpleado { get; set; }
+
+        [Required]
+        public DateTime FechaNacimiento { get; set; }
+        [Required]
+        public DateTime FechaContratacion { get; set; }
+
+        [Required]
+        public decimal Salario { get; set; }
+
+        [StringLength(250)]
+        public string Descripcion { get; set; }
+
+        //Propiedad de navegacion
+        public ICollection<Departamento> Departamentos { get; set; }
+    }
+}
