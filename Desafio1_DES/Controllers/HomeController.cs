b31432de43af719e7456305fc2cb38d2bc190b46
arieldiazdev10 <ariel.ernesto.diaz.carpio@gmail.com>
2026-07-20 22:06:23 -0600

    Agregar archivos de proyecto.

---
+++ Desafio1_DES/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using Desafio1_DES.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Desafio1_DES.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Desafio1_DES/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Desafio1_DES.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
