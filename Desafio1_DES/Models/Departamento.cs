bceb85d9322f0a1851f136c2fb8d2209d2adf684
arieldiazdev10 <ariel.ernesto.diaz.carpio@gmail.com>
2026-07-20 22:36:33 -0600

    Creación de modelo y contexto para entidad Departamento

---
+++ Desafio1_DES/Models/Departamento.cs
@@ -0,0 +1,9 @@
+﻿namespace Desafio1_DES.Models
+{
+    public class Departamento
+    {
+        public int ID { get; set; }//Código único de departamento
+        public string NombreDepartamento { get; set; }//Nombre del departamento
+        public string Descripcion { get; set; }//Descripción
+    }
+}
---
+++ Desafio1_DES/Models/DepartamentosDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Desafio1_DES.Models
+{
+    public class DepartamentosDBContext: DbContext
+    {
+        public DepartamentosDBContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Departamento>Departamentos { get; set; }
+    }
+}
