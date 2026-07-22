c5bf681f2b680e0011da83cada1f074b9b5d82b3
Melany.Bojorquez <bojorquezfabiola8@gmail.com>
2026-07-22 02:45:34 -0600

    Corrige direccion de FK: Empleado tiene DepartamentoId, Departamento tiene coleccion de Empleados

--- Desafio1_DES/Models/Departamento.cs
+++ Desafio1_DES/Models/Departamento.cs
@@ -1,24 +1,15 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-
 namespace Desafio1_DES.Models
 {
     public class Departamento
     {
         public int ID { get; set; }//Código único de departamento
-
         [StringLength(100, MinimumLength = 3)]
         [Required]
         public string NombreDepartamento { get; set; }//Nombre del departamento (Obligatorio)
-
         public string? Descripcion { get; set; }//Descripción (Opcional)
 
-        //Llave foranea
-        [Required]
-        [ForeignKey("Empleado")]
-        public int? EmpleadoId { get; set; }
-
-        //Propiedad de navegación
-        public Empleado Empleado { get; set; }
+        //Propiedad de navegación inversa (un departamento tiene muchos empleados)
+        public ICollection<Empleado> Empleados { get; set; } = new List<Empleado>();
     }
 }
